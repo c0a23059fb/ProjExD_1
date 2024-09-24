449d911a8c7529d41fbca43f5ed11231546f3061
c0a23059fb <c0a23059fb@edu.teu.ac.jp>
2024-09-24 18:04:29 +0900

    演習課題(改善版)

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -24,20 +24,27 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        x = -(tmr % 3200) #練習6
+
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2, [x + 1600, 0]) #練習7
+        screen.blit(bg_img, [x + 3200, 0])
+        screen.blit(bg_img2, [x + 4800, 0])
         key_lst = pg.key.get_pressed() #練習8-3
         kk_x, kk_y = 0, 0
-        if (key_lst[pg.K_LEFT] == False) and (key_lst[pg.K_DOWN] == False) and (key_lst[pg.K_UP] == False) and (key_lst[pg.K_RIGHT] == False): #演習課題1
-            kk_rct.move_ip((-1, 0))
-        else: #演習課題2
-            if key_lst[pg.K_UP]:
-                    kk_y = -2
-            if key_lst[pg.K_DOWN]:
-                    kk_y = 2
-            if key_lst[pg.K_LEFT]:
-                    kk_x = -2
-            if key_lst[pg.K_RIGHT]:
-                    kk_x = 4
-            kk_rct.move_ip((kk_x, kk_y))
+        # if (key_lst[pg.K_LEFT] == False) and (key_lst[pg.K_DOWN] == False) and (key_lst[pg.K_UP] == False) and (key_lst[pg.K_RIGHT] == False): #演習課題1
+        #     kk_rct.move_ip((-1, 0))
+        # else: #演習課題2
+        if key_lst[pg.K_UP]:
+                kk_y -= 1
+        if key_lst[pg.K_DOWN]:
+                kk_y += 1
+        if key_lst[pg.K_LEFT]:
+                kk_x -= 1
+        if key_lst[pg.K_RIGHT]:
+                kk_x += 2
+        kk_x -= 1      
+        kk_rct.move_ip((kk_x, kk_y))
 
         # if key_lst[pg.K_UP]:
         #     kk_rct.move_ip((0, -1))
@@ -48,12 +55,12 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     kk_rct.move_ip((1, 0))
 
-        x = -(tmr % 3200) #練習6
+        # x = -(tmr % 3200) #練習6
 
-        screen.blit(bg_img, [x, 0])
-        screen.blit(bg_img2, [x + 1600, 0]) #練習7
-        screen.blit(bg_img, [x + 3200, 0])
-        screen.blit(bg_img2, [x + 4800, 0])
+        # screen.blit(bg_img, [x, 0])
+        # screen.blit(bg_img2, [x + 1600, 0]) #練習7
+        # screen.blit(bg_img, [x + 3200, 0])
+        # screen.blit(bg_img2, [x + 4800, 0])
         # screen.blit(kk_img, [300, 200]) #練習4
         screen.blit(kk_img, kk_rct) #練習8-5
         pg.display.update()
